83c6441e24369f87f2345e3fd28c2ce7616e3368
Bao <lbx01072004@gmail.com>
2020-07-08 22:15:35 +1000

    Updates

---
+++ precache-manifest.835a3468ce7ac6e6f674973994b88172.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a370f4d2d2e03b98d640ed04eec5a3",
+    "url": "/wk20-react-portfolio/index.html"
+  },
+  {
+    "revision": "f541b2bd635574b5a005",
+    "url": "/wk20-react-portfolio/static/css/main.9398505d.chunk.css"
+  },
+  {
+    "revision": "206a431a885222ef23f9",
+    "url": "/wk20-react-portfolio/static/js/2.16d60001.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wk20-react-portfolio/static/js/2.16d60001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f541b2bd635574b5a005",
+    "url": "/wk20-react-portfolio/static/js/main.d1f8f5b1.chunk.js"
+  },
+  {
+    "revision": "7ecd3cc49d5796e8b8b1",
+    "url": "/wk20-react-portfolio/static/js/runtime-main.bbc653da.js"
+  },
+  {
+    "revision": "2ebcf001c6799fed8de52c8d26d62896",
+    "url": "/wk20-react-portfolio/static/media/Resume.2ebcf001.pdf"
+  },
+  {
+    "revision": "9d48d6ed169e639144aa6cc8980dac7e",
+    "url": "/wk20-react-portfolio/static/media/contact.9d48d6ed.png"
+  },
+  {
+    "revision": "0eede24a3bdaf89b2c0e38fadc1cf1fa",
+    "url": "/wk20-react-portfolio/static/media/czhou.0eede24a.jpg"
+  },
+  {
+    "revision": "6cdfb00d8d468311580fc449ddbdbca2",
+    "url": "/wk20-react-portfolio/static/media/czhou2.6cdfb00d.jpg"
+  },
+  {
+    "revision": "23a083e8db5247c883774ba71105ce84",
+    "url": "/wk20-react-portfolio/static/media/index.23a083e8.png"
+  },
+  {
+    "revision": "7c3b9dcc666b1d471e1e7d23860464ff",
+    "url": "/wk20-react-portfolio/static/media/password-generator.7c3b9dcc.png"
+  },
+  {
+    "revision": "d825fa2f7013f4cdcedf61da7eaa592b",
+    "url": "/wk20-react-portfolio/static/media/phat.d825fa2f.png"
+  },
+  {
+    "revision": "ea6ee589c42c2b2428af49e6fa47c274",
+    "url": "/wk20-react-portfolio/static/media/portfolio.ea6ee589.png"
+  },
+  {
+    "revision": "b0a35a049dcfc38bd0a3e639bd8eeb9f",
+    "url": "/wk20-react-portfolio/static/media/the-future.b0a35a04.png"
+  }
+]);
