e1e68a58ba795e0c556f609a734cef4da5e69442
Bao <lbx01072004@gmail.com>
2020-07-08 22:06:38 +1000

    requirements

--- react-portfolio/src/routes/Home.js
+++ react-portfolio/src/routes/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 import Card from "../components/Card";
 import Footer from "../components/Footer";
 import Image from '../components/Image';
@@ -10,7 +10,7 @@ function Home() {
 
   return(
     <div>
-      <Navbar />
+      <Header />
       <Image />
       <Contact />
       <Card />
